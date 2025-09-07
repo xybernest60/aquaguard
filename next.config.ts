9d5f61c1a4261b10fd95bcb0e375f5d359f9ae80
Xyber Nest <xybernest60@gmail.com>
2025-09-07 14:00:42 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (http://e

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'qkgowclzwaxusyjoktqh.supabase.co',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
